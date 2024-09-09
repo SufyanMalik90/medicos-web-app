3c859b87c814c2f5bc010b787822591c97d48887
sufyanmalik90 <shaikh.arman1239090@gmail.com>
2024-09-09 15:03:01 +0500

    commit alerts and success

--- src/app/ui-elements/alerts/page.tsx
+++ src/app/ui-elements/alerts/page.tsx
@@ -19,8 +19,8 @@ const Alerts = () => {
       <div className="rounded-[10px] bg-white p-4 shadow-1 dark:bg-gray-dark dark:shadow-card md:p-6 xl:p-9">
         <div className="flex flex-col gap-7.5">
           <AlertWarning />
-          <AlertSuccess />
-          <AlertError />
+          {/* <AlertSuccess /> */}
+          {/* <AlertError /> */}
         </div>
       </div>
     </DefaultLayout>
