9035c388282274f56dcba2f5edef805274041ec1
sufyan.malik <sufyan.malik@launchbox.pk>
2024-09-24 19:51:19 +0500

    add main title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export const metadata: Metadata = {
   title:
-    "Next.js E-commerce Dashboard Page | NextAdmin - Next.js Dashboard Kit",
+    "UK Medicos",
   description: "This is Next.js Home page for NextAdmin Dashboard Kit",
 };
 
