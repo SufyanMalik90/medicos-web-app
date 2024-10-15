8486f669809cfbb296db04a686647c3640e6c702
sufyan.malik <sufyan.malik@launchbox.pk>
2024-10-15 15:15:17 +0500

    update view invoices, add filter ui

--- src/components/Tables/TableOne.tsx
+++ src/components/Tables/TableOne.tsx
@@ -4,10 +4,12 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { api } from "../../axios.js";
 import { useRouter } from "next/navigation";
+import DatePickerOne from "../FormElements/DatePicker/DatePickerOne";
 
 const TableOne = () => {
   const [invoices, setInvoices] = useState<any[]>([]);
-  const [searchTerm, setSearchTerm] = useState<string>(""); // Search term state
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false); // State for filter dropdown
 
   const router = useRouter();
 
@@ -47,35 +49,125 @@ const TableOne = () => {
     invoice.customer_id.customer_name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const toggleFilterDropdown = () => {
+    setIsFilterOpen(!isFilterOpen);
+  };
+
   return (
     <div className="rounded-[10px] bg-white px-7.5 pb-4 pt-7.5 shadow-1 dark:bg-gray-dark dark:shadow-card">
       <div className="mb-3 flex items-start justify-between">
         <h4 className="mb-5.5 text-body-2xlg font-bold text-dark dark:text-white">
           Last Invoices
         </h4>
-        <div className="relative">
-          <input
-            type="text"
-            placeholder="Search invoice..."
-            className="rounded-lg dark:bg-gray-700 bg-white py-2 pl-10 pr-4 dark:text-white text-black border-2 dark:border-none placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            value={searchTerm}
-            onChange={handleSearch}
-          />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
-            height="18"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-search absolute left-3 top-2.5 text-gray-400"
-          >
-            <circle cx="11" cy="11" r="8"></circle>
-            <path d="m21 21-4.3-4.3"></path>
-          </svg>
+        <div className="flex gap-2">
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Search invoice..."
+              className="rounded-lg border-2 bg-white py-2 pl-10 pr-4 text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:border-none dark:bg-gray-700 dark:text-white"
+              value={searchTerm}
+              onChange={handleSearch}
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-search absolute left-3 top-2.5 text-gray-400"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </svg>
+          </div>
+          <div className="relative">
+            <div
+              className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-md dark:bg-gray-200 shadow-md"
+              onClick={toggleFilterDropdown}
+            >
+              <svg
+                width="49"
+                height="49"
+                viewBox="0 0 49 49"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <mask
+                  id="mask0_4926_11631"
+                  maskUnits="userSpaceOnUse"
+                  x="0"
+                  y="0"
+                  width="49"
+                  height="49"
+                >
+                  <rect width="49" height="49" fill="#D9D9D9" />
+                </mask>
+                <g mask="url(#mask0_4926_11631)">
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M20.3748 24.2886C20.3997 24.3108 20.4232 24.3316 20.4468 24.3565C21.9408 25.8879 22.7646 27.9163 22.7646 30.0694V35.5857L25.8606 33.8993C26.1043 33.7663 26.2552 33.506 26.2552 33.2194V30.0528C26.2552 27.908 27.0708 25.8865 28.5509 24.3634L35.2483 17.2396C35.6817 16.7785 35.9199 16.1734 35.9199 15.5351V14.2391C35.9199 13.5966 35.4131 13.0746 34.7928 13.0746H14.2022C13.5805 13.0746 13.0737 13.5966 13.0737 14.2391V15.5351C13.0737 16.1734 13.3119 16.7785 13.7452 17.2382L20.3748 24.2886ZM22.2791 38.0014C21.9994 38.0014 21.7225 37.9266 21.4705 37.7771C20.9831 37.4863 20.6909 36.9726 20.6909 36.4022V30.0717C20.6909 28.5002 20.1052 27.02 19.0377 25.886C19.0058 25.8597 18.974 25.8306 18.9463 25.8002L12.2365 18.6652C11.4389 17.8178 11 16.706 11 15.5374V14.2414C11 12.4538 12.4386 11 14.2054 11H34.796C36.5614 11 38 12.4538 38 14.2414V15.5374C38 16.7046 37.5611 17.8151 36.7663 18.6638L30.0551 25.8002C28.9432 26.9466 28.3354 28.4545 28.3354 30.0551V33.2217C28.3354 34.2685 27.7691 35.2266 26.858 35.7251L23.0351 37.8075C22.7969 37.9363 22.538 38.0014 22.2791 38.0014V38.0014Z"
+                    fill="#C1C7C9"
+                  />
+                </g>
+              </svg>
+            </div>
+            {isFilterOpen && (
+              <div className="absolute right-0 mt-2 w-72 rounded-md bg-white p-4 shadow-lg dark:bg-gray-900">
+                <h4 className="mb-2 text-lg font-bold text-black dark:text-white">
+                  Filter Invoices
+                </h4>
+                <hr />
+                {/* <DatePickerOne label="From Date"/> */}
+                <div className="my-2 mb-4">
+                  <label className="mb-1 block text-sm font-bold text-black dark:text-white">
+                    From Date
+                  </label>
+                  <div className="flex items-center rounded-md border bg-gray-100 p-2 dark:border-none dark:bg-gray-800">
+                    <input
+                      type="date"
+                      // value={startDate}
+                      // onChange={(e) => setStartDate(e.target.value)}
+                      className="flex-1 bg-gray-100 align-bottom outline-none dark:bg-gray-800"
+                      placeholder="MM/DD/YYYY"
+                    />
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <label className="mb-1 block text-sm font-bold text-black dark:text-white">
+                    To Date
+                  </label>
+                  <div className="flex items-center rounded-md border bg-gray-100 p-2 dark:border-none dark:bg-gray-800">
+                    <input
+                      type="date"
+                      // value={startDate}
+                      // onChange={(e) => setStartDate(e.target.value)}
+                      className="flex-1 bg-gray-100 outline-none dark:bg-gray-800"
+                      placeholder="MM/DD/YYYY"
+                    />
+                  </div>
+                </div>
+                <div className="flex justify-between gap-2">
+                  <button
+                    className="w-full rounded-md bg-gray-200 px-4 py-2 text-gray-500"
+                    // onClick={handleClear}
+                  >
+                    Clear
+                  </button>
+                  <button
+                    className=" w-full rounded-md bg-[#5750f1] px-4 py-2 text-white"
+                    // onClick={handleApply}
+                  >
+                    Apply
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
@@ -127,7 +219,10 @@ const TableOne = () => {
                 </div>
 
                 <div className="flex items-center justify-center px-2 py-4">
-                  <p className="font-medium text-green-light-1"> {invoice.total}</p>
+                  <p className="font-medium text-green-light-1">
+                    {" "}
+                    {invoice.total}
+                  </p>
                 </div>
 
                 <div className="items-center justify-center px-2 py-4 sm:flex">
