13d8646bc15f8b2369f0026c757a63820994d181
sufyan.malik <sufyan.malik@launchbox.pk>
2024-10-15 16:11:03 +0500

    add update product ui

--- src/components/Tables/TableThree.tsx
+++ src/components/Tables/TableThree.tsx
@@ -1,4 +1,5 @@
 import { Package } from "@/types/package";
+import Image from "next/image";
 
 const packageData: Package[] = [
   {
@@ -28,6 +29,11 @@ const packageData: Package[] = [
 ];
 
 const TableThree = ({products}: any) => {
+
+  const handleViewDetails = (product_id: string) => {
+    console.log("invoices >>", product_id);
+    
+  };
   return (
     <div className="rounded-[10px] bg-white shadow-1 dark:bg-gray-dark dark:shadow-card">
       <div className="px-4 py-6 md:px-6 xl:px-9">
@@ -44,7 +50,10 @@ const TableThree = ({products}: any) => {
           <p className="font-medium">Price</p>
         </div>
         <div className="col-span-1 flex items-center">
-          <p className="font-medium">Quantity</p>
+          <p className="font-medium">Qty</p>
+        </div>
+        <div className="col-span-1 flex items-center">
+          <p className="font-medium">Action</p>
         </div>
         {/* <div className="col-span-1 flex items-center">
         <p className="font-medium">Qty</p>
@@ -85,6 +94,23 @@ const TableThree = ({products}: any) => {
               {product.stock}
             </p>
           </div>
+          <div className="col-span-1 flex items-center">
+                  <div className="flex items-center gap-4">
+                    <button
+                      type="button"
+                      className="hover:text-blue-700"
+                      onClick={() => handleViewDetails(product._id)}
+                    >
+                      <Image
+                        alt="edit-icon"
+                        src="/images/icon/edit.svg"
+                        width={20}
+                        height={20}
+                        className="text-green-900"
+                      />
+                    </button>
+                  </div>
+                </div>
           {/* <div className="col-span-1 flex items-center">
           <p className="text-body-sm font-medium text-dark dark:text-dark-6">
             {product.sold}
